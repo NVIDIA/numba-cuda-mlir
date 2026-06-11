9b306082f3d96df3c54791e7bfb82e3cbf5dd487
Andres Guzman-Ballen <acosmicflamingo@gmail.com>
2026-06-10 20:00:47 -0500

    Generate version tuple via list comprehension approach

--- src/numba_cuda_mlir/numba_cuda/_compat.py
+++ src/numba_cuda_mlir/numba_cuda/_compat.py
@@ -2,9 +2,8 @@
 # SPDX-License-Identifier: BSD-2-Clause
 from cuda import core
 
-# Utilize core._version attributes
-#    __version__ => '1.0.2.dev46+g16df11dab'
-#    __version_tuple__ => (1, 0, 2, 'dev46', 'g16df11dab')
-CUDA_CORE_VERSION = core._version.__version_tuple__
+# core.__version__ returns str like '1.0.1',
+# '1.0.2.dev46+g16df11dab', etc.
+CUDA_CORE_VERSION = tuple(int(i) for i in core.__version__.split(".") if i.isdigit())
 CUDA_CORE_GT_0_6 = CUDA_CORE_VERSION >= (0, 6, 0)
 CUDA_CORE_GE_1_0 = CUDA_CORE_VERSION >= (1, 0, 0)

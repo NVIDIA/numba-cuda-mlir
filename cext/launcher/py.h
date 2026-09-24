d878ce11b8ea171fad2b0fa139ce2efccee8a6ae
Chris Cameron <chrisjamescameron1@gmail.com>
2026-09-24 17:30:04 +1200

    Native launch types participate in garbage collection and kernel families start at one reference

--- cext/launcher/py.h
+++ cext/launcher/py.h
@@ -68,9 +68,17 @@ PyObject* pywrapper_new(PyTypeObject* type, PyObject*, PyObject*) {
 
 template <typename T>
 void pywrapper_dealloc(PyObject* self) {
+    if (PyType_IS_GC(Py_TYPE(self)))
+        PyObject_GC_UnTrack(self);
     PythonWrapper<T>* wrapper = reinterpret_cast<PythonWrapper<T>*>(self);
     wrapper->object.~T();
     Py_TYPE(self)->tp_free(self);
+}
+
+// tp_traverse for wrappers whose T defines traverse(visitproc, void*).
+template <typename T>
+int pywrapper_traverse(PyObject* self, visitproc visit, void* arg) {
+    return py_unwrap<T>(self).traverse(visit, arg);
 }
 
 struct OK_t{};
